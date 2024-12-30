8b7d053d3c84002a489ebeadd1178635c0a723e8
FurryBasket9978 <pgillbama@icloud.com>
2024-12-30 16:33:00 -0600

    finished challenge 2 for day 2

--- Day2Chal2.java
+++ Day2Chal2.java
@@ -25,59 +25,41 @@ public class Day2Chal2 {
     }
 
     public static Boolean isIncreasing(String[] line){
-        Boolean hasAlreadyUsedSkip = false; 
         for (int i = 1; i < line.length; i++){
             Boolean isWithinRange = Integer.parseInt(line[i]) <= Integer.parseInt(line[i-1]) + 3 && Integer.parseInt(line[i]) >= Integer.parseInt(line[i-1]) + 1;
-            if (!isWithinRange && hasAlreadyUsedSkip){ 
+            
+            if (!isWithinRange){
                 return false;
             }
-            else if (!isWithinRange && !hasAlreadyUsedSkip){
-                if (!isIncreaseingWithSkip(line, i)){
-                    return false;
-                }
-            }
         }
+
         return true;
     } 
     
-    public static Boolean isIncreaseingWithSkip(String[] line, int index){
-        line = remove(line, index); 
-        for (int i = 1; i < line.length; i++){
-             Boolean isWithinRange = Integer.parseInt(line[i]) <= Integer.parseInt(line[i-1]) + 3 && Integer.parseInt(line[i]) >= Integer.parseInt(line[i-1]) + 1;
-            if (!isWithinRange){
-                return false;
-            }
-            
-        }
-        return true;
-    }
-
     public static Boolean isDecreasing(String[] line){
-        Boolean hasAlreadyUsedSkip = false; 
         for (int i = 1; i < line.length; i++){
             Boolean isWithinRange = Integer.parseInt(line[i]) <= Integer.parseInt(line[i-1]) - 1 && Integer.parseInt(line[i]) >= Integer.parseInt(line[i-1]) - 3; 
-            if (!isWithinRange && hasAlreadyUsedSkip){
-                return false;
-            }
-            else if (!isWithinRange && !hasAlreadyUsedSkip){
-                if (!isDecreasingWithSkip(line, i)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static Boolean isDecreasingWithSkip(String[] line, int index){
-        line = remove(line, index); 
-        for (int i = 1; i < line.length; i++){
-            Boolean isWithinRange = Integer.parseInt(line[i]) <= Integer.parseInt(line[i-1]) - 1 && Integer.parseInt(line[i]) >= Integer.parseInt(line[i-1]) - 3; 
+            
             if (!isWithinRange){
                 return false;
             }
-            
         }
+
         return true;
+    }
+    
+    public static Boolean checkPermutations (String[] line){
+        if (isIncreasing(line) || isDecreasing(line)){
+            return true;
+        } 
+        
+        for (int i = 0; i < line.length; i++){
+            if (isIncreasing(remove(line, i)) || isDecreasing(remove(line, i))){
+                return true;
+            }
+        }
+
+        return false;
     }
     public static void main (String[] args){
         try{
@@ -85,24 +67,23 @@ public class Day2Chal2 {
             Scanner Reader = new Scanner(input);
 
             int count = 0;
+            long startTime = System.currentTimeMillis();
             while (Reader.hasNextLine()){
                 String data = Reader.nextLine();
                 String[] line = data.split(" ");
 
-                if (isDecreasing(line) || isIncreasing(line)){
-                    printArr(line);
-                    System.out.println("true");
+                if (checkPermutations(line)){
                     count++;
-                }
-                else{
-                    printArr(line);
-                    System.out.println("false");
-                }
+                } 
             
             }
             System.out.println(count);
 
             Reader.close();
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
         }
         catch (FileNotFoundException e){
             System.out.println("File Not Found");
