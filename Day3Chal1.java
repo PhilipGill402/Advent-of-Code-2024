73f063f4ff8115d73bd9fcdba9c906465fa1551f
FurryBasket9978 <pgillbama@icloud.com>
2024-12-30 17:22:40 -0600

    made the regex finder a function to reduce redundancy in the code

--- Day3Chal1.java
+++ Day3Chal1.java
@@ -7,39 +7,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day3Chal1 {
+
+    public static List<String> findMatches (String data, String regex){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(data);
+
+        List<String> matches = new ArrayList<String>();
+
+        while (matcher.find()){
+            matches.add(matcher.group());
+        }
+
+        return matches;
+    }
     public static void main (String[] args){
         try{
             File input = new File("Day3Input.txt");
             Scanner Reader = new Scanner(input);
             
             int total = 0;
+            long startTime = System.currentTimeMillis();
             while (Reader.hasNextLine()){
                 String data = Reader.nextLine();
-
-                Pattern pattern = Pattern.compile("mul\\([0-9]{1,4},[0-9]{1,4}\\)");
-                Matcher matcher = pattern.matcher(data);
-
-                List<String> matches = new ArrayList<String>();
-
-                while (matcher.find()){
-                    matches.add(matcher.group());
-                }
+                String regex = "mul\\([0-9]{1,4},[0-9]{1,4}\\)";
                 
+                List<String> matches = findMatches(data, regex);
+
                 for (int i = 0; i < matches.size(); i++){
-                    Pattern numPattern = Pattern.compile("[0-9]{1,4}");
-                    Matcher numMatcher = numPattern.matcher(matches.get(i));
+                    String numData = matches.get(i);
+                    String numRegex = "[0-9]{1,4}";
 
-                    List<String> numMatches = new ArrayList<String>();
-
-                    while (numMatcher.find()){
-                        numMatches.add(numMatcher.group());
-                    }
+                    List<String> numMatches = findMatches(numData, numRegex);
 
                     total += Integer.parseInt(numMatches.get(0)) * Integer.parseInt(numMatches.get(1));
                 }
             
             }
             System.out.println(total);
+            long endTime = System.currentTimeMillis();
+            System.out.println("That took " + (endTime - startTime) + " milliseconds");
             Reader.close();
         }
         catch (FileNotFoundException e){
